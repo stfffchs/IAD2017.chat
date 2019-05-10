a1e490e2cd976da7812b772e880a107aaa6cafb2
Stefan Huber <sh@signalwerk.ch>
2019-05-10 15:15:07 +0200

    ADD: new modifier on PostItem

--- src/Components/PostItem/index.js
+++ src/Components/PostItem/index.js
@@ -5,7 +5,11 @@ class PostItem extends Component {
   render() {
     const { type, from, text } = this.props;
     return (
-      <div className="PostItem">
+      <div
+        className={`PostItem PostItem--${type} PostItem--${
+          from === "me" ? "me" : "other"
+        }`}
+      >
         <div
           className={`PostItem__Post PostItem__Post--${type} PostItem__Post--${
             from === "me" ? "me" : "other"
